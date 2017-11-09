b4475726e6805947d3c04854c9c089f9f9d1aaab
Refik <Refik@Refik-PC>
2017-11-09 18:53:32 +0100

    beach

---
+++ src/zadaci_08_11_2017/Zadatak3.java
@@ -0,0 +1,21 @@
+package zadaci_08_11_2017;
+
+public class Zadatak3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   int n1 = 0;
+	       int n2 = 1000;
+	       System.out.println("\tBrojevi djelivi sa 4 su : ");
+	      
+	      for (int i = n1; i < n2; i++) {
+	      if ((i % 4 == 0) && (i % 5 != 0)) {
+	    	    System.out.println(i);
+	      }
+	      }
+	          
+	
+	       
+	}
+
+}
